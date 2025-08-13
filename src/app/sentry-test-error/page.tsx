7b8ef86a20f61a1c7f2c8859ca5fa4a7f551eb5a
molmecarl <jinaro@yahoo.com>
2025-08-13 18:09:52 -0400

    chore: remove Sentry test route

--- src/app/sentry-test-error/page.tsx
+++
@@ -1,4 +0,0 @@
-// Test page removed to avoid generating test events in Sentry.
-export default function SentryTestRemoved() {
-  return <div style={{ padding: 24, fontFamily: 'Inter, system-ui, sans-serif' }}>This test route has been removed.</div>;
-}
